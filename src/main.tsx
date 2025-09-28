8c94988d5aa133b86d05c639343fa91078821f5d
Guillaume Gremillet <guillaumeg@bam.tech>
2025-09-28 20:33:32 +0200

    fix: add basename for URLs

--- src/main.tsx
+++ src/main.tsx
@@ -8,29 +8,34 @@ import { Leaderboard } from "./router/Leaderboard";
 import { EnigmaScreen } from "./router/Enigma";
 import { QRCodes } from "./router/QRCodes";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <Root />,
+    },
+    {
+      path: "enigma/:enigmaId",
+      element: <EnigmaScreen />,
+    },
+    {
+      path: "/ghost/:ghostId",
+      element: <Ghost />,
+    },
+    {
+      path: "/leaderboard",
+      element: <Leaderboard />,
+    },
+    {
+      path: "/allCodes",
+      element: <QRCodes />,
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    errorElement: <Root />,
-  },
-  {
-    path: "enigma/:enigmaId",
-    element: <EnigmaScreen />,
-  },
-  {
-    path: "/ghost/:ghostId",
-    element: <Ghost />,
-  },
-  {
-    path: "/leaderboard",
-    element: <Leaderboard />,
-  },
-  {
-    path: "/allCodes",
-    element: <QRCodes />,
-  },
-]);
+    basename: "/halloween-delaf-2025",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
