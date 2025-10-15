9b9bff47667086d1c311df59d84c5797ed0108b3
Guillaume Gremillet <guillaumeg@bam.tech>
2025-10-15 14:16:10 +0200

    feat: add progress bar in leaderboard

--- src/router/Leaderboard.tsx
+++ src/router/Leaderboard.tsx
@@ -2,6 +2,12 @@ import "../index.css";
 import { FloatingGhost } from "../components/FloatingGhost";
 import { useAllUsers } from "../hooks/useAllUsers";
 import { totalScore, useUser } from "../hooks/useUser";
+import ghosts from "../assets/ghosts.json";
+import enigmas from "../assets/enigmas.json";
+import { POINTS_PER_GHOST } from "../hooks/useGhost";
+import { POINTS_PER_ENIGMA } from "../hooks/useEnigma";
+
+const PROGRESS_BAR_HEIGHT = 5;
 
 export const Leaderboard = () => {
   const { user, isLoading: isLoadingUser } = useUser();
@@ -23,6 +29,9 @@ export const Leaderboard = () => {
   const sortedUsers = usersWithTotalScore.sort(
     (a, b) => b.totalScore - a.totalScore
   );
+  const total =
+    ghosts.length * POINTS_PER_GHOST +
+    enigmas.length * (POINTS_PER_GHOST + POINTS_PER_ENIGMA);
 
   return (
     <table
@@ -34,7 +43,7 @@ export const Leaderboard = () => {
         <tr>
           <th>Rang</th>
           <th>Nom</th>
-          <th>Points</th>
+          <th>Points / {total}</th>
         </tr>
       </thead>
       <tbody>
@@ -42,7 +51,33 @@ export const Leaderboard = () => {
           <tr key={u.id}>
             <td>{index + 1}</td>
             <td>{u.id === user.id ? <strong>⭐️ {u.name}</strong> : u.name}</td>
-            <td>{u.totalScore}</td>
+            <td>
+              {u.totalScore}
+              <div
+                style={{
+                  position: "relative",
+                  width: "100%",
+                  height: PROGRESS_BAR_HEIGHT,
+                }}
+              >
+                <div
+                  style={{
+                    width: "100%",
+                    height: PROGRESS_BAR_HEIGHT,
+                    background: "gray",
+                  }}
+                />
+                <div
+                  style={{
+                    width: `${(u.totalScore / total) * 100}%`,
+                    height: PROGRESS_BAR_HEIGHT,
+                    background: "green",
+                    position: "absolute",
+                    left: 0,
+                  }}
+                />
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
